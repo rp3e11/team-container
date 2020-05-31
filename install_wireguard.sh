fa22932f37dde444f532404e16c172f0b39732df
Richard Pichler <richard.pichler@enterango.com>
2020-05-31 15:45:08 +0200

    fixes

--- install_wireguard.sh
+++ install_wireguard.sh
@@ -9,28 +9,28 @@ echo $DIST
 SETUP=false
 
 # install wiredguard, ufw and fail2ban from sources - depends on operating system
-if [ $DIST -eq Ubuntu ]
+if [ "$DIST" == "Ubuntu" ]
 then
-  if [ $RELEASE -eq 18.04 ] 
+  if [ "$RELEASE" == "18.04" ] 
   then
     SETUP=true
     sudo add-apt-repository ppa:wireguard/wireguard
   fi
-  if [ $RELEASE -eq 20.04 ]
+  if [ "$RELEASE" == "20.04" ]
   then
     SETUP=true
   fi
-  if [ $SETUP -eq true ]
+  if [ $SETUP ]
   then
     sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get install wireguard ufw -y
   fi
 fi
 
 # operating system independent setup
-if [ $SETUP -eq true] 
+if [ $SETUP ] 
 then
   wg genkey | sudo tee /etc/wireguard/privatekey | wg pubkey | sudo tee /etc/wireguard/publickey
-  cp wiredguard/wg0.conf wg0.conf
+  cp wireguard/wg0.conf wg0.conf
   sed -i "s/INTF_REPLACE/$(sudo cat /etc/wireguard/privatekey)/" wg0.conf
   sed -i "s/INTF_REPLACE/$(ip -o -4 route show to default | awk '{print $5}')/" wg0.conf
   sudo mv wg0.conf /etc/wireguard/wg0.conf
@@ -39,10 +39,10 @@ then
   sudo systemctl enable wg-quick@wg0
   
   # setup ufw
-  ufw allow proto tcp from any to any port 22
-  ufw -f default deny incoming
-  ufw -f default deny outcoming
-  ufw -f from 10.0.1.1/24 allow
+  ufw allow 22
+  ufw default deny incoming
+  ufw default deny outgoing
+  ufw allow from 10.0.1.1/24 
   ufw allow 51820/udp
 
 fi
